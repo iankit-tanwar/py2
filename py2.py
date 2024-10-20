461819d6b6b8c53842e1c3d8aeeb43a7c1558ca7
Ankit Tanwar <tankitofficial01@gmail.com>
2024-10-20 11:35:49 +0530

    py2

--- py2.py
+++ py2.py
@@ -1,3 +1,16 @@
+
+
 print("welcome to python course")
 
 print("This is another day and another code")
+
+
+#single line comenting 
+if 5 > 2:
+    print("Five is greather than 2")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
